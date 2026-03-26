9472beec99d6d362d60fc83d5255e1ad66882420
Mansi Bhandari <148347130+Thejaggeddevil@users.noreply.github.com>
2026-03-26 17:32:03 +0530

    updated csv

--- app/src/main/java/com/example/babyparenting/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/babyparenting/navigation/AppNavigation.kt
@@ -53,12 +53,11 @@ fun AppNavigation(
     val parentVm:  ParentViewModel  = viewModel()
     val authVm:    AuthViewModel    = viewModel()
 
-  val startDestination = Routes.JOURNEY
-      //when {
-//        authVm.isLoggedIn() && journeyVm.getChildName().isNotBlank() -> Routes.JOURNEY
-//        authVm.isLoggedIn()                                          -> Routes.ONBOARDING
-//        else                                                         -> Routes.LOGIN
-//    }
+  val startDestination = when {
+      authVm.isLoggedIn() && journeyVm.getChildName().isNotBlank() -> Routes.JOURNEY
+       authVm.isLoggedIn()                                          -> Routes.ONBOARDING
+       else                                                         -> Routes.LOGIN
+   }
 
     NavHost(navController = navController, startDestination = startDestination) {
 
